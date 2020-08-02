46b4d4e992b8510dc3ca26fe340bd7cf65c9f917
Silver Miu <giahunghust@gmail.com>
2020-08-02 22:02:30 +0700

    Site updated: 2020-08-02 22:02:29

---
+++ js/main.js
@@ -0,0 +1,22 @@
+// $(function() {
+//   barba.init({
+
+//     sync: true,
+
+//     transitions: [{
+//       async after(data) {
+//         var gitalk = new Gitalk({
+//           clientID: '#{theme.comment.ClientID}',
+//           clientSecret: '#{theme.comment.ClientSecret}',
+//           repo: '#{theme.comment.repo}',
+//           owner: '#{theme.comment.owner}',
+//           admin: ['#{theme.comment.admin}'],
+//           id: md5(location.pathname),
+//           distractionFreeMode: true
+//         });
+//         gitalk.render('vcomments');
+//       },
+//     }]
+//   });
+// });
+
